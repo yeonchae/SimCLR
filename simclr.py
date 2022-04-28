89efe457d49fe8cc98100b3d683a370b4edb13bf
yeonchae <64460370+yeonchae@users.noreply.github.com>
2022-04-28 15:08:11 +0900

    Add files via upload

---
+++ simclr.py
@@ -0,0 +1,124 @@
+import logging
+import os
+import sys
+from torch.nn import DataParallel
+import torch
+import torch.nn.functional as F
+from torch.cuda.amp import GradScaler, autocast
+from torch.utils.tensorboard import SummaryWriter
+from tqdm import tqdm
+from utils.accuracy import accuracy
+
+torch.manual_seed(0)
+
+
+class SimCLR(object):
+
+    def __init__(self, net, loss, loader_train, loader_val, config, scheduler=None):
+
+        super().__init__()
+        self.writer = SummaryWriter()
+        logging.basicConfig(filename=os.path.join(self.writer.log_dir, 'training.log'), level=logging.DEBUG)
+        self.criterion = torch.nn.CrossEntropyLoss().to(self.args.device)
+        self.logger = logging.getLogger("yAwareCL")
+        self.loss = loss
+        self.model = net
+        self.optimizer = torch.optim.Adam(net.parameters(), lr=config.lr, weight_decay=config.weight_decay)
+        self.scheduler = scheduler
+        self.loader = loader_train
+        self.loader_val = loader_val
+        self.device = torch.device("cuda" if config.cuda else "cpu")
+        if config.cuda and not torch.cuda.is_available():
+            raise ValueError("No GPU found: set cuda=False parameter.")
+        self.config = config
+        self.metrics = {}
+
+        if hasattr(config, 'pretrained_path') and config.pretrained_path is not None:
+            self.load_model(config.pretrained_path)
+
+        self.model = DataParallel(self.model).to(self.device)
+
+    def info_nce_loss(self, features):
+
+        labels = torch.cat([torch.arange(self.args.batch_size) for i in range(self.args.n_views)], dim=0)
+        labels = (labels.unsqueeze(0) == labels.unsqueeze(1)).float()
+        labels = labels.to(self.devicee)
+
+        features = F.normalize(features, dim=1)
+
+        similarity_matrix = torch.matmul(features, features.T)
+        # assert similarity_matrix.shape == (
+        #     self.args.n_views * self.args.batch_size, self.args.n_views * self.args.batch_size)
+        # assert similarity_matrix.shape == labels.shape
+
+        # discard the main diagonal from both: labels and similarities matrix
+        mask = torch.eye(labels.shape[0], dtype=torch.bool).to(self.args.device)
+        labels = labels[~mask].view(labels.shape[0], -1)
+        similarity_matrix = similarity_matrix[~mask].view(similarity_matrix.shape[0], -1)
+        # assert similarity_matrix.shape == labels.shape
+
+        # select and combine multiple positives
+        positives = similarity_matrix[labels.bool()].view(labels.shape[0], -1)
+
+        # select only the negatives the negatives
+        negatives = similarity_matrix[~labels.bool()].view(similarity_matrix.shape[0], -1)
+
+        logits = torch.cat([positives, negatives], dim=1)
+        labels = torch.zeros(logits.shape[0], dtype=torch.long).to(self.args.device)
+
+        logits = logits / self.args.temperature
+        return logits, labels
+
+    def train(self, train_loader):
+
+        scaler = GradScaler(enabled=self.args.fp16_precision)
+
+        n_iter = 0
+        logging.info(f"Start SimCLR training for {self.config.nb_epochs} epochs.")
+
+        for epoch_counter in range(self.config.nb_epochs):
+            self.model.train()
+            nb_batch = len(self.loader)
+            training_loss = []
+            pbar = tqdm(total=nb_batch, desc="Training")
+            for images, _ in tqdm(train_loader):
+                images = torch.cat(images, dim=0)
+
+                images = images.to(self.args.device)
+
+                with autocast(enabled=self.args.fp16_precision):
+                    features = self.model(images)
+                    logits, labels = self.info_nce_loss(features)
+                    loss = self.criterion(logits, labels)
+
+                self.optimizer.zero_grad()
+
+                scaler.scale(loss).backward()
+
+                scaler.step(self.optimizer)
+                scaler.update()
+
+                if n_iter % self.args.log_every_n_steps == 0:
+                    top1, top5 = accuracy(logits, labels, topk=(1, 5))
+                    self.writer.add_scalar('loss', loss, global_step=n_iter)
+                    self.writer.add_scalar('acc/top1', top1[0], global_step=n_iter)
+                    self.writer.add_scalar('acc/top5', top5[0], global_step=n_iter)
+                    self.writer.add_scalar('learning_rate', self.scheduler.get_lr()[0], global_step=n_iter)
+
+                n_iter += 1
+
+            # warmup for the first 10 epochs
+            if epoch_counter >= 10:
+                self.scheduler.step()
+            logging.debug(f"Epoch: {epoch_counter}\tLoss: {loss}\tTop1 accuracy: {top1[0]}")
+
+        logging.info("Training has finished.")
+        # save model checkpoints
+        checkpoint_name = 'checkpoint_{:04d}.pth.tar'.format(self.args.epochs)
+        torch.save({
+            'epoch': self.args.epochs,
+            'arch': self.args.arch,
+            'state_dict': self.model.state_dict(),
+            'optimizer': self.optimizer.state_dict(),
+        }, is_best=False, filename=os.path.join(self.writer.log_dir, checkpoint_name))
+        logging.info(f"Model checkpoint and metadata has been saved at {self.writer.log_dir}.")
